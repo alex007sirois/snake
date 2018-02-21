576ef7a9929f6f7d11ef5f2be1ba22744d40aec6
alex007sirois <alex007sirois@gmail.com>
2018-02-21 13:51:06 -0500

    Environement de test

---
+++ test/test.js
@@ -0,0 +1,9 @@
+var expect = chai.expect;
+
+describe("snake game", function() {
+  describe("snake", function() {
+    it("should be in middle", function() {
+      expect(true).to.equal(true);
+    });
+  });
+});
